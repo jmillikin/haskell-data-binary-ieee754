157060198fe6cc9e6b94653fe4851318798f085c
John Millikin <jmillikin@gmail.com>
2009-09-21 17:19:18 +0000

    Add test properties for ``putFloat*'' functions.

--- Tests.hs
+++ Tests.hs
@@ -45,6 +45,10 @@ main = do
 		, props_GetFloat32
 		, props_GetFloat64
 		]
+	runTests "serialising" runOnce . map (run . property) . concat $
+		[ props_PutFloat32
+		, props_PutFloat64
+		]
 	runTests "passthrough" runDeep . map run $
 		[ property $ passthrough putFloat32le getFloat32le
 		, property $ passthrough putFloat32be getFloat32be
@@ -111,19 +115,71 @@ props_GetFloat64 = let check = checkGet getFloat64be getFloat64le in
 	, check [0xFF, 0xF0, 0, 0, 0, 0, 0, 0] (== -inf64)
 	
 	-- NaN
-	, check [0x7F, 0xF8, 0, 0, 0, 0, 0, 0] isNaN
-	, check [0xFF, 0xF8, 0, 0, 0, 0, 0, 0] isNaN
+	, check [0x7F, 0xF8, 0, 0, 0, 0, 0, 0] (and' isNaN (not . isNegativeNaN))
+	, check [0xFF, 0xF8, 0, 0, 0, 0, 0, 0] isNegativeNaN
+	]
+
+props_PutFloat32 = let check = checkPut putFloat32be putFloat32le in
+	[ check [0, 0, 0, 0]   0.0
+	, check [0x80, 0, 0, 0] (-0.0)
+	
+	-- Normalized
+	, check [0x3F, 0x80, 0, 0]   1.0
+	, check [0xBF, 0x80, 0, 0] (-1.0)
+	
+	-- Denormalized
+	, check [0x00, 0x7F, 0xFF, 0xFF]   1.1754942106924411e-38
+	, check [0x80, 0x7F, 0xFF, 0xFF] (-1.1754942106924411e-38)
+	
+	-- Infinity
+	, check [0x7F, 0x80, 0, 0]   inf32
+	, check [0x7F, 0x80, 0, 0] (-inf32)
+	
+	-- NaN
+	, check [0x7F, 0xC0, 0, 0]   nan32
+	, check [0xFF, 0xC0, 0, 0] (-nan32)
+	]
+
+props_PutFloat64 = let check = checkPut putFloat64be putFloat64le in
+	[ check [0, 0, 0, 0, 0, 0, 0, 0]      0.0
+	, check [0x80, 0, 0, 0, 0, 0, 0, 0] (-0.0)
+	
+	-- Normalized
+	, check [0x3F, 0xF0, 0, 0, 0, 0, 0, 0]   1.0
+	, check [0xBF, 0xF0, 0, 0, 0, 0, 0, 0] (-1.0)
+	
+	-- Denormalized
+	, check [0x00, 0x0F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]   2.2250738585072009e-308
+	, check [0x80, 0x0F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF] (-2.2250738585072009e-308)
+	
+	-- Infinity
+	, check [0x7F, 0xF0, 0, 0, 0, 0, 0, 0]   inf64
+	, check [0xFF, 0xF0, 0, 0, 0, 0, 0, 0] (-inf64)
+	
+	-- NaN
+	, check [0x7F, 0xF8, 0, 0, 0, 0, 0, 0]   nan64
+	, check [0xFF, 0xF8, 0, 0, 0, 0, 0, 0] (-nan64)
 	]
 
 checkGet :: Eq a => Get a -> Get a -> [Word8] -> (a -> Bool) -> Bool
-checkGet getBE getLE words f = valid where
+checkGet getBE getLE bytes f = valid where
 	valid = sameResult && B.null remainingBE && f xBE
 	sameResult = remainingBE == remainingLE && xBE == xLE
-	(xBE, remainingBE, _) = runGetState getBE (B.pack words) 0
-	(xLE, remainingLE, _) = runGetState getLE (B.pack (reverse words)) 0
+	(xBE, remainingBE, _) = runGetState getBE (B.pack bytes) 0
+	(xLE, remainingLE, _) = runGetState getLE (B.pack (reverse bytes)) 0
+
+checkPut :: (a -> Put) -> (a -> Put) -> [Word8] -> a -> Bool
+checkPut putBE putLE bytes x = valid where
+	valid = sameResult && bytes == B.unpack bytesBE
+	sameResult = bytesBE == (B.reverse bytesLE)
+	bytesBE = runPut (putBE x)
+	bytesLE = runPut (putLE x)
 
 and' :: (a -> Bool) -> (a -> Bool) -> a -> Bool
 and' f g x = f x && g x
+
+isNegativeNaN :: RealFloat a => a -> Bool
+isNegativeNaN x = isNaN x && (floor x > 0)
 
 -- Verify that the given put and get functions 
 passthrough :: Eq a => (a -> Put) -> Get a -> a -> Bool
@@ -141,5 +197,5 @@ inf64 = read "Infinity"
 nan32 :: Float
 nan32 = read "NaN"
 
-nan64 :: Float
+nan64 :: Double
 nan64 = read "NaN"
